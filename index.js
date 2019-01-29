9080380c556494b2d033c09b31214b7a841f3aeb
Luis Felipe <asp.r2.agottani@gmail.com>
2019-01-29 18:50:38 -0200

    Initialize development

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+function todos(state = [], action) {
+  if (action === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+
 function createStore() {
   let state
+  let listeners = [];
 
   const getState = () => state
 
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
   return (
-    getState
+    getState,
+    subscribe
   )
 }
+
+const store = createStore()
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+const unsubscribe = store.unsubscribe(() => {
+  console.log('The new changed')
+})
